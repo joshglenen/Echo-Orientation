18b02de2885ec8f498c110cf13c387989ad9959b
Josh Glenen <30480135+joshglenen@users.noreply.github.com>
2018-09-21 23:22:56 -0300

    Minor Edit

--- 2D_orientation_using_two_pressure_sensors_WIthK.c
+++ 2D_orientation_using_two_pressure_sensors_WIthK.c
@@ -8,56 +8,10 @@
 //theory: if k = 1, _leftIsLeading will not switch if _leftIsLeading is true initially and robot is rotating ccw
 //theoryL if k = 1, _leftIsLeading will switch if _leftIsLeading is false initially and robot is rotating ccw
 
-int findK()
-{
-	int k = 0;
-	bool initialLeader = leftIsLeading;
-
-	float theta = calculateAsmuth();
-
-	//turn 90 degrees ccw
-	generatePWM(-90);
-	for(int k = 0; k < memRMS; k++)
-		{
-			for(int i = 0; i< memRMSWindow; i++)
-			{
-				recordData();
-			}
-			//we now have our sampled window of memRMSWindow/fs seconds
-			determineRMS(k);
-		}
-
-	bool newLeader = isLeftLeading();
-	if(initialLeader)
-	{
-		if(initialLeader==newLeader) k = 1;
-	}
-	else
-	{
-		if(initialLeader==!newLeader) k = 1;
-	}
-
-	return k;
-}
-
-double calculateAsmuthWithK()
-{
-	int k = findK();
-	double asmuth = 0;
-	asmuth = pow(-1,leftAmpIsHigher);
-	double ratio = speedOfSound * delay / (distanceSeparatingSensors / 1000);
-	asmuth = (asmuth*asin(ratio)+k*PI)*180/PI;
-	return asmuth;
-}
-
 bool isLeftLeading()
 {
 
 	bool _leftIsLeading = false;
-
-	//assume signal is above the noise floor with a reasonable SNR
-	//I am using a fos of 105% to prevent the motor from constantly running. More precise systems can reduce this down to an ideal 100%.
-	float fos = 1.05;
 
 	//which signal is leading?
 	//this is a bit more difficult as there are four cases without using large samples and derivation to determine rise or fall
@@ -88,6 +42,46 @@ bool isLeftLeading()
 
 	}
 	return _leftIsLeading;
+}
+
+int findK()
+{
+	int k = 0;
+	bool initialLeader = leftIsLeading;
+	
+	//turn 90 degrees ccw and resample
+	generatePWM(-90);
+	for(int k = 0; k < memRMS; k++)
+		{
+			for(int i = 0; i< memRMSWindow; i++)
+			{
+				recordData();
+			}
+			//we now have our sampled window of memRMSWindow/fs seconds
+			determineRMS(k);
+		}
+	
+	//get new leader position and apply theory
+	bool newLeader = isLeftLeading();
+	if(initialLeader)
+	{
+		if(initialLeader==newLeader) k = 1;
+	}
+	else
+	{
+		if(initialLeader==!newLeader) k = 1;
+	}
+	return k;
+}
+
+double calculateAsmuthWithK()
+{
+	int k = findK();
+	double asmuth = 0;
+	asmuth = pow(-1,leftAmpIsHigher);
+	double ratio = speedOfSound * delay / (distanceSeparatingSensors / 1000);
+	asmuth = (asmuth*asin(ratio)+k*PI)*180/PI;
+	return asmuth;
 }
 
 
